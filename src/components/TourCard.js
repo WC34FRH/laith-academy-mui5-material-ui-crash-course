51acc1b698b5b14c61e7dee043d0b5ecc4133abf
WC34FRH <wc34frh@gmail.com>
2022-08-11 01:44:43 +0100

    tutorial completed up to 33:24

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,10 +1,33 @@
 import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
 const TourCard = () => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>Hello</Paper>
+      <Paper elevation={3}>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Niagara_Falls%2C_New_York_from_Skylon_Tower.jpg/250px-Niagara_Falls%2C_New_York_from_Skylon_Tower.jpg"
+          alt=""
+          className="img"
+        />
+        <Box paddingX={1}>
+          <Typography variant="subtitle1" component="h2">
+            Immerse into the Falls
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="body2" component="p">
+              5 hours
+            </Typography>
+          </Box>
+        </Box>
+      </Paper>
     </Grid>
   );
 };
