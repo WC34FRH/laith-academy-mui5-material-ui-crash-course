6b3951ee5bce46c915f1c7ec33a49f8822c75d8a
WC34FRH <wc34frh@gmail.com>
2022-08-12 00:31:32 +0100

    tutorial completed up to 44:57

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -2,6 +2,8 @@ import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import Rating from "@mui/material/Rating";
+import { AccessTime } from "@mui/icons-material";
 
 const TourCard = () => {
   return (
@@ -22,8 +24,35 @@ const TourCard = () => {
               alignItems: "center",
             }}
           >
-            <Typography variant="body2" component="p">
+            <AccessTime sx={{ width: 12.5 }} />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
               5 hours
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+            marginTop={3}
+          >
+            <Rating
+              name="read-only"
+              value={4.5}
+              readOnly
+              precision={0.5}
+              size="small"
+            />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              4.5
+            </Typography>
+            <Typography variant="body3" component="p" marginLeft={1.5}>
+              (655 reviews)
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant="h6" component="h3" marginTop={0}>
+              From C $147
             </Typography>
           </Box>
         </Box>
