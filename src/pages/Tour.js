5c19af8f6b4458dc654ffd9b3438e4584d0c8d7a
WC34FRH <wc34frh@gmail.com>
2022-08-13 00:35:41 +0100

    tutorial completed up to 01:27:19

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,3 +1,48 @@
-const Tour = () => <div>hello</div>;
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import ImageCollage from "../components/ImageCollage";
+import CustomizedAccordions from "../components/Accordion";
+import Paper from "@mui/material/Paper";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BasicModal from "../components/Modal";
+
+const Tour = () => (
+  <Container sx={{ width: 900 }}>
+    <Typography variant="h3" component="h1" marginTop={3}>
+      Explore the world in Vegas
+    </Typography>
+    <Box marginTop={3} sx={{ display: "flex" }}>
+      <img
+        src="https://media.timeout.com/images/105124791/750/422/image.jpg"
+        alt=""
+        height={325}
+      />
+      <ImageCollage />
+    </Box>
+    <Box>
+      <Typography variant="h6" component="h4" marginTop={3}>
+        About this ticket
+      </Typography>
+      <Typography variant="paragraph" component="p" marginTop={3}>
+        lorem40
+      </Typography>
+    </Box>
+    <Box marginBottom={10}>
+      <Typography variant="h6" component="h4" marginTop={3} marginBottom={2}>
+        Frequently asked questions
+      </Typography>
+      <CustomizedAccordions />
+    </Box>
+    <Paper
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <BottomNavigation>
+        <BasicModal />
+      </BottomNavigation>
+    </Paper>
+  </Container>
+);
 
 export default Tour;
